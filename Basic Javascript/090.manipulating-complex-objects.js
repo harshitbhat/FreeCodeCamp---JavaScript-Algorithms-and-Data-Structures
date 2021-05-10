fc55bb87955ad70a34aa82ad8015b94f56d95e01
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-05-10 13:40:38 +0530

    Manipulating Complex Objects

---
+++ Basic Javascript/090.manipulating-complex-objects.js
@@ -0,0 +1,16 @@
+var myMusic = [
+  {
+    artist: 'Billy Joel',
+    title: 'Piano Man',
+    release_year: 1973,
+    formats: ['CD', '8T', 'LP'],
+    gold: true,
+  },
+];
+
+myMusic.push({
+  artist: 'Amit Trivedi',
+  title: 'Songs of Faith',
+  release_year: 2020,
+  formats: ['CD', '8T', 'LP'],
+});
