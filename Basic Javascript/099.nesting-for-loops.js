79cbe50ceabafa3d73769daa26d6fde231456ba7
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-05-10 14:50:25 +0530

    Nesting For Loops

---
+++ Basic Javascript/099.nesting-for-loops.js
@@ -0,0 +1,20 @@
+function multiplyAll(arr) {
+  var product = 1;
+  // Only change code below this line
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      product *= arr[i][j];
+    }
+  }
+  console.log(product);
+  // Only change code above this line
+  return product;
+}
+
+const arr = [
+  [1, 2],
+  [3, 4],
+  [5, 6, 7],
+];
+
+multiplyAll(arr);
