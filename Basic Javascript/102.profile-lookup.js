3e4797402e2a9b6479b32fe6a62f301ddb4291ca
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-05-11 10:41:24 +0530

    Profile Lookup

---
+++ Basic Javascript/102.profile-lookup.js
@@ -0,0 +1,44 @@
+// Setup
+var contacts = [
+  {
+    firstName: 'Akira',
+    lastName: 'Laine',
+    number: '0543236543',
+    likes: ['Pizza', 'Coding', 'Brownie Points'],
+  },
+  {
+    firstName: 'Harry',
+    lastName: 'Potter',
+    number: '0994372684',
+    likes: ['Hogwarts', 'Magic', 'Hagrid'],
+  },
+  {
+    firstName: 'Sherlock',
+    lastName: 'Holmes',
+    number: '0487345643',
+    likes: ['Intriguing Cases', 'Violin'],
+  },
+  {
+    firstName: 'Kristian',
+    lastName: 'Vos',
+    number: 'unknown',
+    likes: ['JavaScript', 'Gaming', 'Foxes'],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+  for (let person of contacts) {
+    if (person.firstName === name) {
+      if (person.hasOwnProperty(prop)) {
+        return person[prop];
+      } else {
+        return 'No such property';
+      }
+    }
+  }
+  return 'No such contact';
+  // Only change code above this line
+}
+
+lookUpProfile('Akira', 'likes');
