f0f53a2aa1663af71d1ecd1fb4fb7fb490d2ac84
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-05-14 11:45:10 +0530

    Match All Letters and Numbers

--- Regular expressions/018.match-all-letters-and-numbers.js
+++ Regular expressions/018.match-all-letters-and-numbers.js
@@ -1,3 +1,3 @@
 let quoteSample = 'The five boxing wizards jump quickly.';
-let alphabetRegexV2 = /\w/g; // Change this line
+let alphabetRegexV2 = /\w/g; // \w to check all characters plus numbers, g for all.
 let result = quoteSample.match(alphabetRegexV2).length;
