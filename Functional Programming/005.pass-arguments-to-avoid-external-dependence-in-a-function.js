ff43b2cd2cacb1c8243881b679bb38f06aa59746
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-06-02 20:14:39 +0530

    Pass Arguments to Avoid External Dependence in a Function

---
+++ Functional Programming/005.pass-arguments-to-avoid-external-dependence-in-a-function.js
@@ -0,0 +1,9 @@
+// The global variable
+var fixedValue = 4;
+
+// Only change code below this line
+function incrementer(value) {
+  ++value;
+  return value;
+  // Only change code above this line
+}
