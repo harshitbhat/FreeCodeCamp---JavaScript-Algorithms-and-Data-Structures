6f63bfa3d7530f6f09e41e4a004666a37917f8cb
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-06-06 20:53:38 +0530

    Roman Numeral Converter

---
+++ Projects/002.roman-numeral-converter.js
@@ -0,0 +1,44 @@
+const conversion = {
+  1: 'I',
+  2: 'II',
+  3: 'III',
+  4: 'IV',
+  5: 'V',
+  6: 'VI',
+  7: 'VII',
+  8: 'VIII',
+  9: 'IX',
+  10: 'X',
+  20: 'XX',
+  30: 'XXX',
+  40: 'XL',
+  50: 'L',
+  60: 'LX',
+  70: 'LXX',
+  80: 'LXXX',
+  90: 'XC',
+  100: 'C',
+  200: 'CC',
+  300: 'CCC',
+  400: 'CD',
+  500: 'D',
+  600: 'DC',
+  700: 'DCC',
+  800: 'DCCC',
+  900: 'CM',
+  1000: 'M',
+  2000: 'MM',
+  3000: 'MMM',
+};
+
+function convertToRoman(num) {
+  let ans = '';
+  let i = 0;
+  while (num) {
+    const val = (num % 10) * 10 ** i;
+    if (val != 0) ans = conversion[val] + ans;
+    i++;
+    num = Math.floor(num / 10);
+  }
+  return ans;
+}
